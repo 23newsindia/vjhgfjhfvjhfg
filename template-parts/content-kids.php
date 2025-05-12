015fd503b7cee5a84dd4146f5089b40329c73740
23newsindia <162028696+23newsindia@users.noreply.github.com>
2025-05-12 17:56:21 +0530

    Create content-kids.php

---
+++ template-parts/content-kids.php
@@ -0,0 +1,55 @@
+<div class="woocommerce-gender-page men-page">
+    <!-- Slider Shortcode -->
+    <div class="gender-slider">
+        <?php echo do_shortcode('[aws_slider slug="nm"]'); ?>
+    </div>
+    
+    <!-- Countdown Timer with Spacing -->
+    <div class="countdown-timer-section" style="margin: 2px 0; padding: 2px 0;">
+        <?php echo do_shortcode('[product_carousel slug="d"]'); ?>
+    </div>
+</div>
+
+
+
+<div class="woocommerce-gender-page b-page2">
+      <!-- Countdown Timer with Spacing -->
+    <div class="countdown-timer-section" style="margin: 2px 0; padding: 2px 0;">
+        <?php echo do_shortcode('[abc_banner slug="ggfegeg"]'); ?>
+    </div>
+
+
+
+
+<div class="woocommerce-gender-page men-page3">
+    <!-- Slider Shortcode -->
+    <div class="gender-slider">
+        <?php echo do_shortcode('[category_grid slug="e"]'); ?>
+    </div>
+
+
+
+
+
+<div class="woocommerce-gender-page men-page2">
+    <!-- Slider Shortcode -->
+    <div class="gender-slider">
+        <?php echo do_shortcode('[product_carousel slug="basd"]'); ?>
+    </div>
+
+
+
+
+
+
+<div class="woocommerce-gender-page men-page2">
+    <!-- Slider Shortcode -->
+    <div class="gender-slider">
+        <?php echo do_shortcode('[abc_banner slug="ggfegeg"]'); ?>
+    </div>
+  
+  
+  
+  
+  
+</div>
